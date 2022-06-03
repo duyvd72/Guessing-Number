89f333949e35dcf51431e3fed6f8a93867f848a0
Duy Vũ <92747088+duyvd72@users.noreply.github.com>
2022-06-03 15:07:50 +0700

    refactor done

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
 'use strict'
 
-/*console.log(document.querySelector('.message').textContent)
-document.querySelector('.message').textContent = '🎉 Correct Number!'
-
-document.querySelector('.number').textContent = 10
-document.querySelector('.score').textContent = 15
-
-document.querySelector('.guess').value = 23
-console.log(document.querySelector('.guess').value)
-*/
-
 let secretNumber = Math.trunc(Math.random() * 20) + 1
 let score = Number(document.querySelector('.score').textContent)
 let highScore = 0
+
+const displayMessage = message => {
+    document.querySelector('.message').textContent = message
+}
 
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value)
@@ -24,18 +18,18 @@ document.querySelector('.check').addEventListener('click', function () {
         // When the guess is wrong
     } else if (guess !== secretNumber) {
         if (score > 1) {
-            document.querySelector('.message').textContent = guess > secretNumber ? '📈 Too high!' : '📉 Too low!'
+            displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!')
             score--
             document.querySelector('.score').textContent = score
         } else {
-            document.querySelector('.message').textContent = '💥 You lost the game!'
+            displayMessage('💥 You lost the game!')
             document.querySelector('.score').textContent = 0
             document.querySelector('body').style.backgroundColor = '#b93030'
         }
         // When player wins 
     } else if (guess === secretNumber) {
         document.querySelector('.number').textContent = secretNumber
-        document.querySelector('.message').textContent = '🎉 Correct Number!'
+        displayMessage('🎉 Correct Number!')
         document.querySelector('body').style.backgroundColor = '#60b347'
         document.querySelector('.number').style.width = '30rem'
         if (score > highScore) highScore = score
@@ -48,7 +42,7 @@ document.querySelector('.again').addEventListener('click', function () {
     document.querySelector('.score').textContent = score
     secretNumber = Math.trunc(Math.random() * 20) + 1
     document.querySelector('.number').textContent = '?'
-    document.querySelector('.message').textContent = 'Start guessing...'
+    displayMessage('Start guessing...')
     document.querySelector('.guess').value = ''
     document.querySelector('body').style.backgroundColor = '#222'
     document.querySelector('.highscore').textContent = highScore
